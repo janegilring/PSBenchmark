5b28cf58dcc6d57d0658f2af1c182fb7908c5d32
Jan Egil Ring <janegilring@users.noreply.github.com>
2016-09-29 18:19:44 +0200

    Initial commit

---
+++ Functions/Measure-CopyPerformance.ps1
@@ -0,0 +1,58 @@
+﻿function Measure-CopyPerformance {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true,ValueFromPipelinebyPropertyName = $true)]
+        [Alias('Destination')]
+        [ValidateNotNullOrEmpty()]
+        [string] $Path,
+
+        [Parameter()]
+        [ValidateRange(0,[uint32]::MaxValue)]
+        [uint32] $Size = 20MB
+    )
+
+    if (Test-Path -Path $Path) {
+        
+        $tmpFolder = [System.IO.Path]::GetTempPath()
+        $tmpFileName = [System.IO.Path]::GetRandomFileName()
+        $tmpFile = Join-Path -Path $tmpFolder -ChildPath $tmpFileName
+        $destinationPath = Join-Path -Path $Path -ChildPath $tmpFileName
+
+        New-File -Path $tmpFile -Size $Size
+        
+        try {
+
+            $sendTest = Measure-Command {
+                Copy-Item -Path $tmpFile -Destination $destinationPath -ErrorAction Stop
+            }
+
+            Remove-Item -Path $tmpFile -Force
+
+            $receiveTest = Measure-Command {
+                Copy-Item -Path $destinationPath -Destination $tmpFile -ErrorAction Stop
+            }
+
+            Remove-Item -Path $tmpFile -Force
+            Remove-Item -Path $destinationPath -Force
+
+            $sendMbps = [Math]::Round(((($Size * 8) / $sendTest.TotalSeconds) / 1000000),2)
+            $receiveMbps = [Math]::Round(((($Size * 8) / $receiveTest.TotalSeconds) / 1000000),2)
+
+            Write-Output ([PSCustomObject] [Ordered] @{
+                Path = $Path
+                SendMbps = $sendMbps
+                ReceiveMbps = $receiveMbps
+            })
+
+        }
+        catch {
+            Write-Warning $_.Exception.Message
+        }
+
+    }
+
+    else {
+        Write-Warning "Unable to connect to $Path"
+    }
+
+}
---
+++ Functions/New-File.ps1
@@ -0,0 +1,23 @@
+﻿function New-File {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Path,
+
+        [Parameter()]
+        [ValidateRange(0,[uint32]::MaxValue)]
+        [uint32] $Size = 0
+    )
+
+    try {
+        $fs = New-Object -TypeName System.IO.FileStream -ArgumentList ($Path, [System.IO.FileMode]::CreateNew)
+        $fs.SetLength($Size)
+    }
+    catch {
+        Write-Warning $_.Exception.Message
+    }
+    finally {
+        $fs.Close()
+    }
+}
